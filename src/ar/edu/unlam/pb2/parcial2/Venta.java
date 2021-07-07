2e46528bf4794c9c472ac96e4142a718a398a6dd
Camila <camilabelen906@gmail.com>
2021-07-07 10:16:09 -0300

    tercer commit

--- src/ar/edu/unlam/pb2/parcial2/Venta.java
+++ src/ar/edu/unlam/pb2/parcial2/Venta.java
@@ -2,12 +2,14 @@ package ar.edu.unlam.pb2.parcial2;
 
 public class Venta {
 
+	private Integer numeroVenta;
 	private Integer dniDelComprador;
 	private String nombreDelComprador;
 
 	public Venta(Integer dniDelComprador, String nombreDelComprador) {
 		this.dniDelComprador  = dniDelComprador;
 		this.nombreDelComprador = nombreDelComprador;
+		this.numeroVenta = 0;
 	}
 	
 	public Double getImporte() {
@@ -30,7 +32,41 @@ public class Venta {
 		this.nombreDelComprador = nombreDelComprador;
 	}
 
+	public Integer getNumeroVenta() {
+		return numeroVenta;
+	}
 
+	public void setNumeroVenta(Integer numeroVenta) {
+		this.numeroVenta = numeroVenta;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((numeroVenta == null) ? 0 : numeroVenta.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Venta other = (Venta) obj;
+		if (numeroVenta == null) {
+			if (other.numeroVenta != null)
+				return false;
+		} else if (!numeroVenta.equals(other.numeroVenta))
+			return false;
+		return true;
+	}
+
+
+	
 	
 	
 
